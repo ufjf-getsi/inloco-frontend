f24a7f8d6d9c8bceed6d87e99c1cc8f4ed30e0a6
Lucas Duarte <lucasduartechaves@hotmail.com>
2022-10-23 02:29:45 -0300

    Create component to return projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
+import { Project } from "./components/Project";
+import axios from "axios";
+
+import "./styles/main.css";
+
+interface Project {
+  id: string;
+  title: string;
+  description: string;
+}
+
 function App() {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    axios("http://localhost:3333/projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-10">
+      <h1 className="text-5xl text-red-800 font-bold mt-5">In Loco</h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        {projects.map((project) => {
+          return (
+            <Project
+              key={project.id}
+              title={project.title}
+              description={project.description}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
