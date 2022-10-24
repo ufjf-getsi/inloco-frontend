7cec6c7bc1ca1be2f978a1a098c57d26313a75de
Augusto Castilho <castilho@ice.ufjf.br>
2022-10-24 18:16:56 -0300

    Creating form to add project

--- src/components/CreateProjectModal.tsx
+++ src/components/CreateProjectModal.tsx
@@ -5,9 +5,14 @@ export function CreateProjectModal() {
     <Dialog.Portal>
       <Dialog.Overlay className="bg-black/60 inset-0 fixed">
         <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
-          <Dialog.Title className="text-3xl font-black text-white">
+          <Dialog.Title className="text-3xl font-black text-white mb-6 text-center">
             Crie um novo projeto{" "}
           </Dialog.Title>
+          <form className="flex flex-col gap-4" method="POST" action="/projects">
+            <input type="text" placeholder="Nome do projeto" className="rounded mb-2 text-white bg-black/20 p-4" />
+            <input type="text" placeholder="Descrição" className="rounded mb-2 text-white bg-black/20 p-4" />
+            <button type="submit" className="rounded text-3xl font-black text-white hover:bg-black/20 p-4">Submit</button>
+          </form>
         </Dialog.Content>
       </Dialog.Overlay>
     </Dialog.Portal>
