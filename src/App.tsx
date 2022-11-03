cd5cf51988e103e26b03283d19ac30ca514e1cb4
Augusto Castilho <castilho@ice.ufjf.br>
2022-11-03 20:43:08 -0300

    Fix: React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./components/pages/HomePage";
+import { ProjectPage } from "./components/pages/ProjectPage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path={"projetos/:id"} element={<ProjectPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
