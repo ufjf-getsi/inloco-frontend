68ba1841dec94d0c3a52cede43cba6640fe825df
Gabriel Malosto <devgabmal@gmail.com>
2022-11-16 14:51:59 -0300

    Show previous data on edit Project form

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
-import { ProjectPage } from "./pages/ProjectPage";
+import { ViewProject } from "./pages/Project/ViewProject";
 import { CreateProject } from "./pages/Project/CreateProject";
 
 import "@cloudscape-design/global-styles/index.css";
 import "./styles/main.css";
+import { EditProject } from "./pages/Project/EditProject";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<HomePage />} />
-        <Route path={"projects/:id"} element={<ProjectPage />} />
         <Route path={"projects/"} element={<CreateProject />} />
+        <Route path={"projects/:id"} element={<ViewProject />} />
+        <Route path={"projects/:id/edit"} element={<EditProject />} />
       </Routes>
     </BrowserRouter>
   );
